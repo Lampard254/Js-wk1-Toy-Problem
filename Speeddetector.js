88a6ce6676607101e3d82d70ff62f9e1caa21724
Lampard254 <142304041+Lampard254@users.noreply.github.com>
2023-09-26 09:36:59 +0300

    Update Speeddetector.js

--- Speeddetector.js
+++ Speeddetector.js
@@ -1,1 +1,19 @@
-
+function calculateSpeed() {
+    const speedLimit = 70;
+    const pointsPerKmAboveLimit = 5;
+    
+    let carSpeed = 80;
+    //checks whether the carSpeed is less than 70. If it is, it returns "OK" 
+    if (carSpeed < speedLimit) {
+      return = 'Ok';
+    } else {
+    // driver receives 1 demerit point for every 5 km/h over the speed limit of 70 km/h 
+      const demeritPoints = Math.floor((carSpeed - speedLimit) / pointsPerKmAboveLimit);
+    //checks whether the number of points exceeds 12. If it does, the result is set to "License suspended"   
+      if (demeritPoints > 12) {
+        return = 'License suspended';
+      } else {
+        return = ('Points: ${demeritPoints}') ;
+      }
+    }
+  }
