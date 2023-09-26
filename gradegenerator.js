935aa2ecff148f1bdc47dab114b8fd65c54af409
Lampard254 <142304041+Lampard254@users.noreply.github.com>
2023-09-26 09:13:27 +0300

    Update gradegenerator.js

--- gradegenerator.js
+++ gradegenerator.js
@@ -1,1 +1,15 @@
+function calculateGrade() {
+    let studentScore = "100";
+    let grade;
 
+    if (studentScorecore > 79) {
+      grade = "A";
+    } else if (studentScore >= 60) {
+      grade = "B";
+    } else if (studentScore >= 50) {
+      grade = "C";
+    } else if (studentScore >= 40) {
+      grade = "D";
+    } else {
+      grade = "E";
+    }
