78a50063d12e220eab95fe0a30f03cb374876390
Lampard254 <142304041+Lampard254@users.noreply.github.com>
2023-10-03 13:12:04 +0300

    Update gradegenerator.js

--- gradegenerator.js
+++ gradegenerator.js
@@ -1,7 +1,7 @@
 // The calculateGrade function uses a simple if-else statement to determine the grade based 
 // on the value of the marks input. Once the grade is determined, it is displayed by the variable 
 // #grade 
-function calculateGrade() {
+function calculateGrade(studentScore) {
     let studentScore = "100";
     let grade;
 
